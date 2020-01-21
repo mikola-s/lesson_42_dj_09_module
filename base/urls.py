c1a5e3f891c3b0bc565bf621fcd1cd07690df633
mikola-s <suharev.nv@gmail.com>
2020-01-21 16:00:57 +0200

    many edits

--- base/urls.py
+++ base/urls.py
@@ -17,14 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
-
 urlpatterns = [
     path('', include('shop.urls')),
     path('admin/', admin.site.urls),
 ]
 
-
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)), ]
 
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)), ]
