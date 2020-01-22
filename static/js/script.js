6e560f997998f1255dd5ffc07e3755d10579f249
mikola-s <suharev.nv@gmail.com>
2020-01-22 15:46:34 +0200

    product update

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,3 @@
 $("li.nav-item a.nav-link[href='" + location.pathname + "']").addClass('active')
 
 
-
-$(".card-img-top").map(function () {
-    $(this).on('load', function () {
-        console.log('111')
-    })
-})
